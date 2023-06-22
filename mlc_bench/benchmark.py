bb8e724b58990cd395c008f2c78d33e4a3c34e9d
Xiyou Zhou <xiyou@octoml.ai>
2023-06-22 00:05:15 +0000

    Enable relax func benchmarking.

--- mlc_bench/benchmark.py
+++ mlc_bench/benchmark.py
@@ -1,4 +1,4 @@
-from typing import Callable, Union, Tuple, List
+from typing import Callable, Union, Tuple, List, Dict, Any
 
 import tvm
 from tvm.ir import IRModule
@@ -6,18 +6,75 @@ from tvm.tir import PrimFunc
 from tvm.meta_schedule.testing.tune_utils import generate_input_data
 
 import pandas as pd
+import random
 
-pd.set_option("display.max_colwidth", None)
-pd.set_option("display.width", None)
-pd.set_option("display.max_colwidth", None)
+from .extraction import extract_func_info, get_func_name_from_gv
+
+COLUMNS = [
+    "Time(us)",
+    #    "Std(us)",
+    "Weight",
+    "WxTime(ms)",
+]
+
+
+def dedup(func: Callable):
+    def wrapped(*args, **kwargs):
+        factor = func(*args, **kwargs)
+        fail_count = 0
+        while factor in wrapped.used and fail_count < 10:
+            factor = func(*args, **kwargs)
+            fail_count += 1
+        wrapped.used.append(factor)
+        return factor
+
+    wrapped.used = []
+    return wrapped
+
+
+@dedup
+def default_dym_var_sample_func(
+    dym_var_dict: Dict[tvm.relax.expr.Call, str]
+) -> Dict[tvm.relax.expr.Call, int]:
+    results = {}
+    for var in dym_var_dict:
+        if dym_var_dict[var] == "int32" or "int64":
+            results[var] = 2 ** random.randint(2, 10)
+        else:
+            raise TypeError(
+                "Unsupported dynamic shape variable type: " + dym_var_dict[var]
+            )
+    return results
+
+
+def val_value_str(sample: Dict[tvm.relax.expr.Call, int]) -> str:
+    return ", ".join([f"{k}={v}" for k, v in sample.items()])
+
+
+def populuate_input_shape(
+    input_infos: List[Tuple[tvm.relax.TensorStructInfo]],
+    dym_var_sample: Dict[tvm.relax.expr.Call, int],
+) -> List[Tuple[Tuple[int], str]]:
+    results = []
+    for input_info in input_infos:
+        shape = []
+        if isinstance(input_info, tvm.relax.struct_info.ShapeStructInfo):
+            # scalar input
+            results.append((dym_var_sample[input_info.values[0]], "scalar"))
+        else:
+            for dim in input_info.shape:
+                if isinstance(dim, tvm.tir.IntImm):
+                    shape.append(dim.value)
+                else:
+                    shape.append(dym_var_sample[dim])
+            results.append((tuple(shape), input_info.dtype))
+    return results
 
 
 class MLCBench:
     """Benchmarking PrimFuncs or IRModules with Dynamic Input Shapes."""
 
-    df: pd.DataFrame = pd.DataFrame(
-        columns=["Name", "Input Info", "Time(ms)", "Std(ms)"]
-    )
+    df: pd.DataFrame = pd.DataFrame(columns=COLUMNS)
 
     @staticmethod
     def benchmark(
@@ -25,6 +82,8 @@ class MLCBench:
         input_shape_gen_func: Callable[[], Tuple[int]],
         target: str = "nvidia/nvidia-a10g",
         dev: tvm.runtime.Device = tvm.cuda(),
+        number=1,
+        repeat=1,
     ) -> Tuple[List[Tuple[Tuple[int], str]], float, float]:
         if isinstance(mod_or_func, PrimFunc):
             func = mod_or_func
@@ -38,38 +97,151 @@ class MLCBench:
             target = tvm.target.Target(target)
         input_infos = input_shape_gen_func()
         input_tensors = []
+        scalar_input_tensors = []
         for input_shape, input_dtype in input_infos:
-            input_tensors.append(
-                tvm.nd.array(generate_input_data(input_shape, input_dtype), device=dev)
-                if input_shape != ()
-                else 1  # random integer
-            )
+            if input_dtype == "scalar":
+                assert isinstance(input_shape, int)
+                scalar_input_tensors.append(input_shape)
+            else:
+                input_tensors.append(
+                    tvm.nd.array(
+                        generate_input_data(input_shape, input_dtype), device=dev
+                    )
+                )
+        input_tensors.extend(scalar_input_tensors)
         rt_mod = tvm.build(mod, target=target)
-        result = rt_mod.time_evaluator(global_symbol, dev=dev, number=10, repeat=10)(
-            *input_tensors
-        )
+        result = rt_mod.time_evaluator(
+            global_symbol, dev=dev, number=number, repeat=repeat
+        )(*input_tensors)
         return input_infos, result.median, result.std
 
     @staticmethod
     def record(
-        func_name: str,
-        input_infos: List[Tuple[Tuple[int], str]],
-        median: float,
-        std: float,
+        **kwargs,
     ):
         MLCBench.df = MLCBench.df._append(
-            {
-                "Name": func_name,
-                "Input Info": input_infos,
-                "Time(ms)": median * 1000,
-                "Std(ms)": std * 1000,
-            },
+            kwargs,
             ignore_index=True,
         )
 
     @staticmethod
     def show():
         print(MLCBench.df)
+        print("\n")
+
+    @staticmethod
+    def clear():
+        MLCBench.df = pd.DataFrame(columns=COLUMNS)
+
+    @staticmethod
+    def sample_input_shape(
+        prim_funcs: Dict[tvm.ir.GlobalVar, List[Tuple[tvm.relax.expr.Call, str]]],
+        get_var_val_func: Callable[[tvm.relax.expr], int] = lambda x: random.randint(
+            1, 100
+        ),
+    ) -> Dict[tvm.ir.GlobalVar, List[Tuple[Tuple[int], str]]]:
+        rvs = {}
+        results = {}
+        for prim_func_gv in prim_funcs.items():
+            for func_args, _ in prim_funcs[prim_func_gv]:
+                for arg in func_args:
+                    if isinstance(arg, tvm.relax.ShapeStructInfo):
+                        for var in arg:
+                            rvs[var] = 0
+                    elif isinstance(arg, tvm.relax.struct_info.TensorStructInfo):
+                        for var in arg.shape:
+                            if not isinstance(var, tvm.tir.IntImm):
+                                rvs[var] = 0
+                    else:
+                        for sub_arg in arg.fields:
+                            for var in sub_arg.shape:
+                                if not isinstance(var, tvm.tir.IntImm):
+                                    rvs[var] = 0
+        for var in rvs:
+            rvs[var] = get_var_val_func()
+        for prim_func_gv in prim_funcs.items():
+            results[prim_func_gv] = []
+            for func_args, weight in prim_funcs[prim_func_gv]:
+                for arg in func_args:
+                    if isinstance(arg, tvm.relax.ShapeStructInfo):
+                        shape = []
+                        for var in arg:
+                            shape.append(rvs[var])
+                        results[prim_func_gv].appeend((tuple(shape), weight))
+                    elif isinstance(arg, tvm.relax.struct_info.TensorStructInfo):
+                        shape = []
+                        for var in arg.shape:
+                            shape.append(rvs[var])
+                        results[prim_func_gv].appeend((tuple(shape), weight))
+                    else:
+                        for sub_arg in arg.fields:
+                            shape = []
+                            for var in sub_arg.shape:
+                                if not isinstance(var, tvm.tir.IntImm):
+                                    shape.append(rvs[var])
+
+    @staticmethod
+    def benchmark_relax_func(
+        mod: tvm.ir.IRModule,
+        relax_func: Union[tvm.ir.GlobalVar, str] = None,
+        sample_number: int = 2,
+        target: str = "nvidia/nvidia-a10g",
+        dev: tvm.runtime.Device = tvm.cuda(),
+        dym_var_sample_func: Callable[
+            [Dict[tvm.relax.expr.Call, str]],
+            Dict[tvm.relax.expr.Call, int],
+        ] = default_dym_var_sample_func,
+    ):
+        relax_funcs, dynamic_var_dict = extract_func_info(mod)
+
+        if relax_func is None:
+            for gv in relax_funcs:
+                MLCBench.benchmark_relax_func(
+                    mod,
+                    gv,
+                    sample_number,
+                    target,
+                    dev,
+                    dym_var_sample_func,
+                )
+        else:
+            if isinstance(relax_func, str):
+                for gv in relax_funcs:
+                    if get_func_name_from_gv(gv) == relax_func:
+                        relax_func = gv
+                        break
+                if not isinstance(relax_func, tvm.ir.GlobalVar):
+                    raise ValueError(
+                        f"Cannot find relax function with name {relax_func}"
+                    )
+            for _ in range(sample_number):
+                MLCBench.clear()
+                dym_var_sample = dym_var_sample_func(dynamic_var_dict[relax_func])
+                for functor in relax_funcs[relax_func]:
+                    for args, weight in relax_funcs[relax_func][functor]:
+                        input_infos, median, std = MLCBench.benchmark(
+                            mod[functor],
+                            input_shape_gen_func=lambda: populuate_input_shape(
+                                args, dym_var_sample
+                            ),
+                            target=target,
+                            dev=dev,
+                        )
+                        MLCBench.record(
+                            **{
+                                f"PrimFuncs in {get_func_name_from_gv(relax_func)}": get_func_name_from_gv(
+                                    functor
+                                ),
+                                f"InputInfo({val_value_str(dym_var_sample)})": ", ".join(
+                                    [str(w) for w in args]
+                                ),
+                                "Time(us)": median * 1e6,
+                                # "Std(us)": std * 1e6,
+                                "Weight": weight,
+                                "WxTime(ms)": median * weight * 1e3,
+                            }
+                        )
+                MLCBench.show()
 
 
 if __name__ == "__main__":
--- mlc_bench/extraction.py
+++ mlc_bench/extraction.py
@@ -1,36 +1,122 @@
-from typing import List, Tuple, Callable
-import tvm
-import tvm.relax
-import dill
+from typing import List, Dict, Union, Tuple, Callable
+from pathlib import Path
+import cloudpickle
 
-IMPORTS = """
 import tvm
-import dill
-from mlc_bench.benchmark import MLCBench
+
+IMPORTS = """import pickle
+
+import tvm
+from tvm import relax
 from tvm.script import tir as T
+
+from mlc_bench.benchmark import MLCBench
 """
 
-MAIN = """
-if __name__ == "__main__":
+MAIN = """if __name__ == "__main__":
     bench = MLCBench()
     for _ in range(SAMPLE_NUMBER):
         input_infos, median, std = bench.benchmark(
             main, input_shape_gen_func, "llvm -num-cores=4", tvm.cpu()
         )
-        bench.record(FUNC_NAME, input_infos, median, std)
+        bench.record(
+            RELAX_FUNC_NAME,
+            PRIM_FUNC_NAME,
+            input_infos,
+            median,
+            std,
+            WEIGHT,
+            WEIGHT*median
+        )
     bench.show()
 """
 
 
+def counted(func: Callable):
+    def wrapped(*args, **kwargs):
+        wrapped.count += 1
+        return func(*args, **kwargs)
+
+    wrapped.count = -1
+    return wrapped
+
+
+def extract_shape(
+    arg: Union[tuple, list, tvm.relax.Tuple, tvm.relax.ShapeStructInfo]
+) -> List[tvm.relax.ShapeStructInfo]:
+    if isinstance(arg, (tuple, list, tvm.relax.Tuple)):
+        results = []
+        for sub_arg in arg:
+            results.extend(extract_shape(sub_arg))
+        return results
+    else:
+        return [arg.struct_info]
+
+
+def prim_func_usage_gen(
+    mod: tvm.ir.IRModule,
+) -> Tuple[tvm.ir.GlobalVar, tvm.ir.GlobalVar, List[tvm.relax.ShapeStructInfo]]:
+    for gv, func in mod.functions.items():
+        if isinstance(func, tvm.relax.Function):
+            for block in func.body.blocks:
+                for binding in block.bindings:
+                    if isinstance(binding.value, tvm.relax.expr.Call):
+                        raw_args = binding.value.args
+                        functor = raw_args[0]
+                        if isinstance(functor, tvm.ir.GlobalVar):
+                            if isinstance(mod.functions[functor], tvm.tir.PrimFunc):
+                                args = extract_shape(raw_args[1:]) + extract_shape(
+                                    binding.value
+                                )
+                                yield gv, functor, args
+
+
+def extract_dynamic_var(
+    func_dict: Dict,
+) -> Dict[tvm.ir.GlobalVar, Dict[tvm.relax.expr.Call, str]]:
+    dynamic_var_dict = {}
+    for gv in func_dict:
+        dynamic_var_dict[gv] = {}
+        for functor in func_dict[gv]:
+            for arg_list, _ in func_dict[gv][functor]:
+                for arg in arg_list:
+                    if isinstance(arg, tvm.relax.TensorStructInfo):
+                        for v in arg.shape.values:
+                            if isinstance(v, tvm.tir.Var):
+                                dynamic_var_dict[gv][v] = v.dtype
+                    elif isinstance(arg, tvm.relax.ShapeStructInfo):
+                        for v in arg.values:
+                            if isinstance(v, tvm.tir.Var):
+                                dynamic_var_dict[gv][v] = v.dtype
+                    else:
+                        raise NotImplementedError
+    return dynamic_var_dict
+
+
+def extract_func_info(
+    mod: tvm.ir.IRModule,
+) -> Dict[tvm.ir.GlobalVar, Dict[tvm.ir.GlobalVar, List[Tuple[List, int]]]]:
+    """Extract function information from a relax module."""
+
+    def update_records(records, new_args):
+        for i, (args, count) in enumerate(records):
+            if new_args == args:
+                records[i] = (args, count + 1)
+                return
+        records.append((new_args, 1))
+
+    relax_func_dict = {}
+    for gv, functor, args in prim_func_usage_gen(mod):
+        if isinstance(functor, tvm.ir.GlobalVar):
+            if not gv in relax_func_dict:
+                relax_func_dict[gv] = {}
+            if not functor in relax_func_dict[gv]:
+                relax_func_dict[gv][functor] = []
+            update_records(relax_func_dict[gv][functor], args)
+    return relax_func_dict, extract_dynamic_var(relax_func_dict)
+
+
 def get_shape_gen_func(func_args: List[Tuple[tvm.relax.expr.Call, str]]):
-    def counted(func: Callable):
-        def wrapped(*args, **kwargs):
-            wrapped.count += 1
-            return func(*args, **kwargs)
-
-        wrapped.count = -1
-        return wrapped
-
     def produce_shape(shape: tvm.relax.expr.ShapeExpr, count: int) -> Tuple[int]:
         produced = []
         for var in shape:
@@ -76,11 +162,13 @@ def get_shape_gen_func(func_args: List[Tuple[tvm.relax.expr.Call, str]]):
     return input_shape_gen_func
 
 
-def extract_func(
+def extract_prim_func(
     model_name: str,
-    func_name: str,
+    relax_func_name: str,
+    prim_func_name: str,
     func: tvm.tir.PrimFunc,
-    func_args: List[Tuple[List[Tuple[tvm.relax.expr.Call, str]], int]],
+    func_args: List[Tuple[tvm.relax.expr.Call, str]],
+    weight: int,
     file_path: str,
     category: int = -1,
     sample_number: int = 5,
@@ -88,129 +176,41 @@ def extract_func(
     file = open(file_path, "w")
     print(IMPORTS, file=file)
     print(f'MODEL_NAME = "{model_name}"', file=file)
-    print(f'FUNC_NAME = "{func_name}"', file=file)
+    print(f'RELAX_FUNC_NAME = "{relax_func_name}"', file=file)
+    print(f'PRIM_FUNC_NAME = "{prim_func_name}"', file=file)
     print(f"FUNC_HASH = {tvm.ir.structural_hash(func)}", file=file)
-    print(f"WEIGHT = {func_args[0][1]}", file=file)
+    print(f"WEIGHT = {weight}", file=file)
     print(f"CAT = {category}", file=file)
     print(f"SAMPLE_NUMBER = {sample_number}\n", file=file)
     print(
-        f"input_shape_gen_func = dill.loads({dill.dumps(get_shape_gen_func(func_args[0][0]))})",
+        f"input_shape_gen_func = pickle.loads({cloudpickle.dumps(get_shape_gen_func(func_args))})",
         file=file,
     )
-    print(func.script(), file=file)
+    print(func.script() + "\n", file=file)
     print(MAIN, file=file)
-    # func.show()
+
+
+def get_func_name_from_gv(gv: tvm.ir.GlobalVar) -> str:
+    return gv.astext().split("@")[1] if "@" in gv.astext() else gv.astext()
 
 
 def extract_from_relax(mod: tvm.ir.IRModule, model_name: str, file_path: str):
-    prim_funcs = {}
-
-    for gv, func in mod.functions.items():
-        if isinstance(func, tvm.tir.PrimFunc):
-            prim_funcs[gv] = {"func": func, "args": []}
-
-    for gv, func in mod.functions.items():
-        if isinstance(func, tvm.relax.Function):
-            for block in func.body.blocks:
-                for binding in block.bindings:
-                    if isinstance(binding.value, tvm.relax.expr.Call):
-                        args = binding.value.args
-                        gv = args[0]
-                        if gv in prim_funcs:
-                            assert isinstance(gv, tvm.ir.GlobalVar)
-                            args = [arg.struct_info for arg in args[1:]] + [
-                                binding.value.struct_info
-                            ]
-                            new_args = True
-                            for i in range(len(prim_funcs[gv]["args"])):
-                                arg, count = prim_funcs[gv]["args"][i]
-                                if arg == args:
-                                    prim_funcs[gv]["args"][i] = (arg, count + 1)
-                                    new_args = False
-                                    break
-                            if new_args:
-                                prim_funcs[gv]["args"].append((args, 1))
-
-    for gv in prim_funcs:
-        func_name = gv.astext().split("@")[1] if "@" in gv.astext() else gv.astext()
-        extract_func(
-            model_name=model_name,
-            func_name=func_name,
-            func=prim_funcs[gv]["func"],
-            func_args=prim_funcs[gv]["args"],
-            file_path=f"{file_path}/{func_name}.py",
-        )
-
-
-def extract_shape(arg) -> List:
-    if isinstance(arg, (tuple, list, tvm.relax.Tuple)):
-        r = []
-        for a in arg:
-            r.extend(extract_shape(a))
-        return r
-    else:
-        return [arg.struct_info]
-
-
-def prim_func_usage_gen(mod: tvm.ir.IRModule):
-    for gv, func in mod.functions.items():
-        if isinstance(func, tvm.relax.Function):
-            for block in func.body.blocks:
-                for binding in block.bindings:
-                    if isinstance(binding.value, tvm.relax.expr.Call):
-                        raw_args = binding.value.args
-                        functor = raw_args[0]
-                        if isinstance(functor, tvm.ir.GlobalVar):
-                            if isinstance(mod.functions[functor], tvm.tir.PrimFunc):
-                                args = extract_shape(raw_args[1:]) + extract_shape(binding.value)
-                                yield gv, functor, args
-
-
-def extract_dynamic_var(func_dict):
-    dynamic_var_dict = {}
-    for gv in func_dict:
-        print(gv)
-        dynamic_var_dict[gv] = set()
-        for functor in func_dict[gv]:
-            for arg_list, _ in func_dict[gv][functor]:
-                for arg in arg_list:
-                    if isinstance(arg, tvm.relax.TensorStructInfo):
-                        for v in arg.shape.values:
-                            if isinstance(v, tvm.tir.Var):
-                                dynamic_var_dict[gv].add((v.name, v.dtype))
-                    elif isinstance(arg, tvm.relax.ShapeStructInfo):
-                        for v in arg.values:
-                            if isinstance(v, tvm.tir.Var):
-                                dynamic_var_dict[gv].add((v.name, v.dtype))
-                    else:
-                        raise NotImplementedError
-    return dynamic_var_dict
-
-
-def extract(mod: tvm.ir.IRModule):
-    def update_records(records, new_args):
-        for i, (args, count) in enumerate(records):
-            if new_args == args:
-                records[i] = (args, count + 1)
-                return
-        records.append((new_args, 1))
-
-    prim_func_dict = {}
-    relax_func_dict = {}
-    for gv, functor, args in prim_func_usage_gen(mod):
-        if isinstance(functor, tvm.ir.GlobalVar):
-            if not gv in relax_func_dict:
-                relax_func_dict[gv] = {}
-            if not functor in relax_func_dict[gv]:
-                relax_func_dict[gv][functor] = []
-            if not functor in prim_func_dict:
-                prim_func_dict[functor] = []
-            update_records(prim_func_dict[functor], args)
-            update_records(relax_func_dict[gv][functor], args)
-    dynamic_var_dict = extract_dynamic_var(relax_func_dict)
-    print(prim_func_dict)
-    print(relax_func_dict)
-    print(dynamic_var_dict)
+    relax_funcs = extract_func_info(mod)
+    Path(file_path).mkdir(parents=True, exist_ok=True)
+    for relax_func_gv in relax_funcs:
+        relax_func_name = get_func_name_from_gv(relax_func_gv)
+        for prim_func_gv in relax_funcs[relax_func_gv]:
+            prim_func_name = get_func_name_from_gv(prim_func_gv)
+            for func_args, weight in relax_funcs[relax_func_gv][prim_func_gv]:
+                extract_prim_func(
+                    model_name=model_name,
+                    relax_func_name=relax_func_name,
+                    prim_func_name=prim_func_name,
+                    func=mod[prim_func_gv],
+                    func_args=func_args,
+                    weight=weight,
+                    file_path=f"{file_path}/{relax_func_name}_{prim_func_name}.py",
+                )
 
 
 if __name__ == "__main__":
